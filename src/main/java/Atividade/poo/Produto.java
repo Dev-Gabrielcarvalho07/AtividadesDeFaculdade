4fd5e3cd3d98839a8d4502300ab5757d7e777a66
Gabriel Carvalho <senaigabrielcarvalho@gmail.com>
2025-11-06 13:47:26 -0300

    Atividades de POO 07/10/ *Uso de Get e Set.

--- src/main/java/Atividade/poo/Produto.java
+++ src/main/java/Atividade/poo/Produto.java
@@ -9,6 +9,37 @@ package Atividade.poo;
  * @author gabri
  */
 public class Produto {
+    public String nome;
+    private double preco;
+    protected int quantidade;
+    public Produto(String nome, int quantidade){
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+    public void venderProduto(int unidades){
+        if(unidades > quantidade){
+            System.out.println("ERRO!, unidades vendidas maiores que a quantidade em estoque!");
+            
+        }
+        else{
+            unidades = unidades - quantidade;
+            
+        }
+        
+    }
+    public double getPreco(){
+        return preco;
+    }
+    public void setPreco(double preco){
+        this.preco = preco;
+    }
     
+    public void exibirInfo(){
+        System.out.println("Nome do produto: " + nome);
+        
+        System.out.println("Quantidade em estoque: " + quantidade);
+        
+        
+    }
     
 }
