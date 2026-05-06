9a01f883d78b2171815540be4424c798478d2a38
Gabriel Carvalho <senaigabrielcarvalho@gmail.com>
2026-05-06 20:06:55 -0300

    Create Forma class to calculate area

---
+++ src/main/java/Atividade/poo/Forma/Forma.java
@@ -0,0 +1,9 @@
+public class Forma{
+    double calculo;
+    public Forma( double calculo){
+        this.calculo = calculo;
+    }
+    public void calcularArea(){
+        System.out.println("Calculo da area:" +  calculo);
+    }
+}
